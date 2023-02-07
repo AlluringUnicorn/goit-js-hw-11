06b6967e8dd0602c3e32b0e08d31338898f0950c
Mariia Khapun <mariakhapun@gmail.com>
2023-02-07 20:25:09 +0000

    Homework commit

--- src/API.js
+++ src/API.js
@@ -3,14 +3,22 @@ const axios = require('axios').default;
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '33430670-3151596af5f0d850f5d459d27';
 
-export async function getImages(query) {
-  try {
-    const response = await axios.get(
-      `${BASE_URL}?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
-    );
-    console.log(response.data);
-    return response.data;
-  } catch (error) {
-    console.error(error);
+export default class API {
+  constructor() {
+    this.page = 1;
+    this.query = '';
+  }
+
+  async getImages() {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}?key=${KEY}&q=${this.query}&per_page=40&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`
+      );
+      console.log(response.data);
+      this.page += 1;
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,78 @@
-import { getImages } from './API';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import API from './API';
 
 const searchButton = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const gallery = document.getElementById('gallery');
+const loadMore = document.getElementById('load-button');
+
+const api = new API();
 
 searchButton.addEventListener('click', onSearchClick);
+loadMore.addEventListener('click', onLoadMore);
+
+const modal = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function onLoadMore() {
+  api
+    .getImages()
+    .then(data => {
+      const images = data.hits;
+      const quantity = data.totalHits;
+
+      if (images.length === 0) {
+        Notiflix.Notify.failure(
+          'We are sorry, but you have reached the end of search results'
+        );
+        loadMore.hidden = true;
+        return;
+      }
+
+      Notiflix.Notify.success(`Hooray! We found ${quantity} images.`);
+
+      const markup = images.reduce(
+        (markup, image) => createImageCard(image) + markup,
+        ''
+      );
+      return markup;
+    })
+    .then(markup => {
+      gallery.insertAdjacentHTML('beforeend', markup);
+      modal.refresh();
+      scroll();
+    });
+}
 
 function onSearchClick(event) {
   event.preventDefault();
   clearMarkup();
+  loadMore.hidden = true;
 
-  const query = inputEl.value;
+  api.page = 1;
+  api.query = inputEl.value.trim();
 
-  getImages(query).then(data => {
+  if (api.query === '') {
+    clearMarkup();
+    return;
+  }
+
+  api.getImages().then(data => {
     const images = data.hits;
+    const quantity = data.totalHits;
+    loadMore.hidden = false;
 
     if (images.length === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
+      loadMore.hidden = true;
       return;
     }
-    const quantity = data.totalHits;
 
     Notiflix.Notify.success(`Hooray! We found ${quantity} images.`);
 
@@ -36,13 +86,14 @@ function onSearchClick(event) {
 
 function createImageCard({
   webformatURL,
+  largeImageURL,
   tags,
   likes,
   views,
   comments,
   downloads,
 }) {
-  return `<div class="photo-card">
+  return `<a class="gallery__item" href="${largeImageURL}"><div class="photo-card">
   <img src="${webformatURL}" alt="${tags}" loading="lazy" width="340px" height="220px"  />
   <div class="info">
     <p class="info-item">
@@ -62,13 +113,24 @@ function createImageCard({
       ${downloads}
     </p>
   </div>
-</div>`;
+</div></a>`;
 }
 
 function updateMarkup(markup) {
   gallery.innerHTML = markup;
+  modal.refresh();
 }
 
 function clearMarkup() {
   gallery.innerHTML = '';
+}
+
+function scroll() {
+  const { height: cardHeight } =
+    gallery.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
